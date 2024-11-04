914a427ee1d9ddb654feaff6c09f241079b35f10
RAJKUMAR <RAJKUMAR@DESKTOP-2N1409P>
2024-11-04 15:30:07 +0530

    hu

--- C2TC_RAJKUMAR/src/com/tns/ifet/dayone/IdentifiersDemo.java
+++ C2TC_RAJKUMAR/src/com/tns/ifet/dayone/IdentifiersDemo.java
@@ -4,6 +4,10 @@ public class IdentifiersDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int number=20;
+		System.out.println("value of a number:"+number);
+		String studentname="rajkumar";
+		System.out.println("value is"+studentname); 
 
 	}
 
